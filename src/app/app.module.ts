4cb6efc805562dd2d9c2e95915de6e3fe19e5ca1
kobanogit <mlme@disroot.org>
2018-05-04 14:07:47 +0200

    init projet front OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { Router, RouterOutlet } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+const routes: Routes = [
+  // { path: 'login', component: LoginComponent },
+  // { path : 'calendar', component: CalendarComponent },
+  { path: '', redirectTo: 'app', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
+  exports: [ RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
