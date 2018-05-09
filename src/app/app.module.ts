7f407f6f736a4bb927ba62622d3c9ef10a4fd382
kobanogit <mlme@disroot.org>
2018-05-09 14:30:58 +0200

    angular V6 avec page accueil externe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PageLandingComponent } from './page-landing/page-landing.component';
+
+const routes: Routes = [
+  { path: 'landing-page', component: PageLandingComponent },
+  { path: '',
+    redirectTo: 'landing-page',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
+  exports: [ RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
