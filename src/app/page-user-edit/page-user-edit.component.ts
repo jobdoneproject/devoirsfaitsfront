c14711ff0b9a6c36d80f644a26b44eb559ed430f
kobanogit <mlme@disroot.org>
2018-05-11 14:27:13 +0200

    ajout page vierge edition user

---
+++ src/app/page-user-edit/page-user-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-user-edit',
+  templateUrl: './page-user-edit.component.html',
+  styleUrls: ['./page-user-edit.component.css']
+})
+export class PageUserEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
