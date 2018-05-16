390027756da8243ed22f118b5371d5f2bc9f9da4
Paul Ferreira Silva <paul.ferreirasilva@wildcodeschool.fr>
2018-05-16 12:00:46 +0200

    Added environments handling

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import {User} from "../model/model.user";
 import {Http} from "@angular/http";
 import {AppComponent} from "../app.component";
+import {environment} from '../../environments/environment';
+
 
 @Injectable()
 export class AccountService {
   constructor(public http: Http) { }
 
   createAccount(User: User ){
-    return this.http.post(AppComponent.API_URL+'/account/register',User)
+    return this.http.post(environment.API_URL+'/account/register',User)
       .map(resp=>resp.json());
   }
 }
