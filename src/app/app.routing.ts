59d3bc847054e0736b0a60d2495d4d8f756f947e
kobanogit <mlme@disroot.org>
2018-05-17 18:48:42 +0200

    maj sidebar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -16,13 +16,18 @@ const appRoutes: Routes = [
   { path: 'bienvenue', component: PageLandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent ,canActivate: [UrlPermission] },
+  // { path: 'profile', component: ProfileComponent ,canActivate: [UrlPermission] },
+  // { path: 'eleves', component: ElevesComponent ,canActivate: [UrlPermission] },
   { path: 'eleves', component: ElevesComponent ,canActivate: [UrlPermission] },
-  { path: 'eleves', component: ElevesComponent ,canActivate: [UrlPermission] },
+  { path: 'profile',
+    component: ContainerComponent, canActivate: [UrlPermission],
+    children: [{path: '', component: ProfileComponent, outlet: 'connected'}]
+  },
   { path: 'edition-utilisateur/:id',
     component: ContainerComponent, canActivate: [UrlPermission],
     children: [{path: '', component: PageUserEditComponent, outlet: 'connected'}]
   },
+
   { path: '', redirectTo: 'bienvenue', pathMatch: 'full' },
 
   // otherwise redirect to profile
--- src/app/components/container-sidebar/container-sidebar.component.ts
+++ src/app/components/container-sidebar/container-sidebar.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import {User} from "../../model/model.user";
+import {Router} from "@angular/router";
+import {AppComponent} from "../../app.component";
+import {AuthService} from "../../services/auth.service";
+import { Observable } from 'rxjs';
+import {environment} from '../../../environments/environment';
+
+
 @Component({
   selector: 'app-container-sidebar',
   templateUrl: './container-sidebar.component.html',
@@ -7,9 +16,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerSidebarComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+  administrateur: boolean;
+  errorMessage:string;
+  idEtablissement: number;
+  url: string;
+  listEleve: Observable<any>;
+
+  constructor(public authService: AuthService, public router: Router,private http: Http) { 
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.idEtablissement = this.currentUser.idEtablissement;
+    
+    console.log("this.currentUser.privilege : " + this.currentUser.privilege);
+    if (this.currentUser.privilege == "Administrateur"){
+      
+      this.administrateur = true;
+    }
+  }
 
   ngOnInit() {
   }
+
 
 }
