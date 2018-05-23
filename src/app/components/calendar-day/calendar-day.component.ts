d2435c862caac2fe39a67cf252db5a641115db49
laurent bernabé <Laurent_Bernabe_student2018@wilder.school>
2018-05-23 21:38:52 +0200

    Le composant jour calendrier peut recevoir une valeur étiquette pour le jour.

--- src/app/components/calendar-day/calendar-day.component.ts
+++ src/app/components/calendar-day/calendar-day.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'calendar-day',
   templateUrl: './calendar-day.component.html',
-  styleUrls: ['./calendar-day.component.scss']
+  styleUrls: ['./calendar-day.component.scss'],
+  inputs: ['dayLabel : day-label']
 })
 export class CalendarDayComponent implements OnInit {
+
+  private dayLabel: String;
 
   constructor() { }
 
