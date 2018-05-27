705f9e2dc41632689c98842020b8e8cebb1a2e56
sebaurel <sebaurel@free.fr>
2018-05-27 17:05:29 +0200

    ajout nom etablissement

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { User } from "../../model/model.user";
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { map} from 'rxjs/operators';
+import {environment} from '../../../environments/environment';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-container',
@@ -6,8 +13,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
+  currentUser: User;
+  url: string;
+  etablissements: Observable<any> ;
+  nomEtablissement:string;
+  constructor(public authService: AuthService,private http: Http) { 
 
-  constructor() { }
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+    this.url = environment.API_URL+"/etablissement/"+this.currentUser.idEtablissement;
+    this.etablissements = this.http.get(this.url).pipe(map((resp: Response)=>resp.json()));
+    console.log(this.etablissements); 
+
+    this.etablissements.forEach(etablissement => {
+      console.log('foreach1');
+      this.nomEtablissement = etablissement.nomEtablissement;
+    });
+  }
 
   ngOnInit() {
   }
