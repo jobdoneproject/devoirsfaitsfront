1620691b573d228f463f8157b5e49eeb91492bc8
laurent bernabé <Laurent_Bernabe_student2018@wilder.school>
2018-05-30 12:27:19 +0200

    Ajout du bouton poubelle dans les créneaux.

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -28,7 +28,7 @@ export class ProfileComponent implements OnInit {
 
     /////////////////////////////////////////
     this.year = 2018;
-    this.weekNumber = 23;
+    this.weekNumber = 24;
     /////////////////////////////////////////
 
     const attributesAreMissing = this.weekNumber === undefined || this.year === undefined;
