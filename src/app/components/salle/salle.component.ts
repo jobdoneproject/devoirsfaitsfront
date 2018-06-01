ffde98da873efcd3b5ab43239531c254e3a520f6
Thomas Petitjean <contact@petitjeanthomas.fr>
2018-06-01 14:34:00 +0200

    crea des pages pour gestion des salles

---
+++ src/app/components/salle/salle.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, ViewEncapsulation, IterableDiffers, Input, EventEmitter } from '@angular/core';
+import { User } from "../../model/model.user";
+import { Router } from "@angular/router";
+import { AppComponent } from "../../app.component";
+import { AuthService } from "../../services/auth.service";
+import { Observable } from 'rxjs';
+import { map} from 'rxjs/operators';
+import { MatListOption, MatSelectionList } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Time } from '@angular/common';
+import * as moment from 'moment';
+import { environment } from '../../../environments/environment';
+import { CreneauService } from '../../services/creneau.service';
+import { UserService } from "../../services/user.service";
+import { RoomService } from "../../services/room.service";
+import { Room } from '../../model/model.room';
+
+
+@Component({
+  selector: 'app-salle',
+  templateUrl: './salle.component.html',
+  styleUrls: ['./salle.component.scss']
+})
+export class SalleComponenet implements OnInit {
+
+  roomservice: RoomService;
+  currentUser: User;
+  administrateur: boolean;
+  errorMessage: string;
+  listSalle: Observable<any>;
+  @Input() nom_salle: String;
+  nomDisponibles = [];
+  filterParNom: String;
+  titre: String = "Création d'un créneau";
+  myControl: FormControl = new FormControl();
+  filteredEleve: Observable<any[]>;
+
+
+  constructor(private courseservice: CreneauService, public authService: AuthService, public router: Router, private userService:UserService) {
+
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    console.log("this.currentUser.privilege : " + this.currentUser.privilege);
+
+    if (this.currentUser.privilege == "Administrateur") {
+      this.administrateur = true;
+    }
+
+    this.listSalle = this.roomservice.getAll(this.currentUser.idEtablissement);
+
+    this.listSalle.forEach(arrayNomUtilisateur => {
+      arrayNomUtilisateur.forEach(salle => {
+        if (this.nomDisponibles.indexOf(salle.nom) == -1) {
+          this.nomDisponibles.push(salle.nom);
+        }
+      })
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onChangeNom(optionDuMenu) { this.filterParNom = optionDuMenu; }
+
+  createSalle(nom:String) {
+    this.roomservice.createNew(this.currentUser.idEtablissement, nom);
+  }
+
+  deleteSalle (salle: Room) {
+    this.roomservice.deleteSelected(this.currentUser.idEtablissement,salle.id);
+  }
+}
