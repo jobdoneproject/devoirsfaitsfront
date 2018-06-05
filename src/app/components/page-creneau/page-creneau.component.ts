90a206c408827eefd3ae272f634a1dbdd4189a13
Thomas Petitjean <contact@petitjeanthomas.fr>
2018-06-05 17:20:18 +0200

    merge avec branche bruno

--- src/app/components/page-creneau/page-creneau.component.ts
+++ src/app/components/page-creneau/page-creneau.component.ts
@@ -4,7 +4,7 @@ import { Router } from "@angular/router";
 import { AppComponent } from "../../app.component";
 import { AuthService } from "../../services/auth.service";
 import { Observable } from 'rxjs';
-import { map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { MatListOption, MatSelectionList } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -16,7 +16,8 @@ import { environment } from '../../../environments/environment';
 import { CreneauService } from '../../services/creneau.service';
 import { UserService } from "../../services/user.service";
 import { Room } from "../../model/model.room"
-
+import { CourseSlot } from "../../model/model.courseslot";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-page-creneau',
@@ -28,7 +29,7 @@ export class PageCreneauComponent implements OnInit {
 
   selectedEleves: User[] = [];
   selectedProfesseurs: User[] = [];
-  selectedProfesseur: User[] = [];
+  selectedProfesseur: any;
   currentUser: User;
   administrateur: boolean;
   errorMessage: string;
@@ -43,9 +44,11 @@ export class PageCreneauComponent implements OnInit {
   myControl: FormControl = new FormControl();
   filteredEleve: Observable<any[]>;
   salleSelected: Room;
+  creneau: Observable<CourseSlot>;
+  private url: string;
 
 
-  constructor(private courseservice: CreneauService, public authService: AuthService, public router: Router, private userService:UserService) {
+  constructor(private httpClient: HttpClient, private courseservice: CreneauService, public authService: AuthService, public router: Router, private userService: UserService) {
 
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     console.log("this.currentUser.privilege : " + this.currentUser.privilege);
@@ -55,29 +58,38 @@ export class PageCreneauComponent implements OnInit {
     }
 
     this.listProfesseur = this.userService.getUsers("professeur", this.currentUser.idEtablissement);
-    this.listEleve =  this.userService.getUsers("eleve", this.currentUser.idEtablissement);
+    this.listEleve = this.userService.getUsers("eleve", this.currentUser.idEtablissement);
 
     this.listEleve.forEach(arrayNomUtilisateur => {
       arrayNomUtilisateur.forEach(utilisateur => {
         if (this.nomDisponibles.indexOf(utilisateur.nom) == -1) {
           this.nomDisponibles.push(utilisateur.nom);
         }
+        if (this.nomDisponibles.indexOf(utilisateur.classe) == -1) {
+          this.nomDisponibles.push(utilisateur.classe);
+        }
       })
     });
+
+    this.url = environment.API_URL+"/etablissement/1/creneaux/2";
+    this.creneau = this.httpClient.get<CourseSlot>(this.url);
   }
 
   addEleveToSelected() { this.selectedEleves.push(this.myControl.value); }
 
-  addProfesseurToSelected(selectedProfesseur) { this.selectedProfesseurs.push(selectedProfesseur); console.log(selectedProfesseur); }
+  addProfesseurToSelected(selectedProfesseur) {
+    this.selectedProfesseurs.push(selectedProfesseur);
+    console.log(selectedProfesseur);
+  }
 
   majTitre() { this.titre = "Création du créneau du " + this.date_creneau.toString(); }
 
   onSend() {
     this.courseservice.createSlot(moment(this.date_creneau + " " + this.heure_debut).unix(),
-                                  moment(this.date_creneau + " " + this.heure_fin).unix(),
-                                  this.selectedEleves,
-                                  this.selectedProfesseurs,
-                                  this.salleSelected );
+      moment(this.date_creneau + " " + this.heure_fin).unix(),
+      this.selectedEleves,
+      this.selectedProfesseurs,
+      this.salleSelected);
   }
 
   ngOnInit() {
@@ -95,4 +107,20 @@ export class PageCreneauComponent implements OnInit {
       this.selectedEleves.splice(index, 1);
     }
   }
+
+  enleverProfesseur(professeur: User) {
+    const index: number = this.selectedProfesseurs.indexOf(professeur);
+    if (index !== -1) {
+      this.selectedProfesseurs.splice(index, 1);
+    }
+  }
+
+  onCancel() {
+    this.date_creneau = undefined;
+    this.heure_debut = undefined;
+    this.heure_fin = undefined;
+    this.selectedProfesseurs = undefined;
+    this.selectedEleves = undefined;
+  }
+
 }
