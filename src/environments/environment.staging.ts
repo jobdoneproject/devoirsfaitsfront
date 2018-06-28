0e62d89a254fe2ca733bd0a293438d4afb65e4c5
richard <richard@zaclys.net>
2018-06-28 10:55:12 +0200

    update staging for clevercloud

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_URL : "http://app-28286c7a-6571-43b1-96b0-6352d129ffdd.cleverapps.io/devoirsfaits/",
 };
