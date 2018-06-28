5ac2a4d660e168fa349460116508321e1f12187b
richard <richard@zaclys.net>
2018-06-28 15:04:52 +0200

    change build

--- build_staging.sh
+++ build_staging.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 echo "-- BEGINNING STAGING CUSTOM BUILD SCRIPT EXECUTION FOR DEVOIRSFAITS --"
-echo "--> npm i npm@latest -g"
-npm i npm@latest -g
-echo "--> ng build --configuration=staging"
+echo "-- ->ng build --configuration=staging"
 ng build --configuration=staging
 echo "-- END STAGING CUSTOM BUILD SCRIPT EXECUTION FOR DEVOIRSFAITS --"
 
