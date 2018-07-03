0f894f23501a8346a073d13ce209b6a03a80fa1c
richard <richard@zaclys.net>
2018-07-03 12:57:12 +0200

    build_staging modifiyed and CC_RUn_cOMMN+AND

--- build_staging.sh
+++ build_staging.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "-- BEGINNING STAGING CUSTOM BUILD SCRIPT EXECUTION FOR DEVOIRSFAITS --"
-echo "-- ->npm install"
-npm install
-echo "-- END npm install --"
+echo "-- ->npm ng build --configuration=staging --base-href=/"
+ng build --configuration=staging --base-href=/
+echo "-- END npm ng build --configuration=staging --base-href=/ --"
 echo "-- END STAGING CUSTOM BUILD SCRIPT EXECUTION FOR DEVOIRSFAITS --"
 
