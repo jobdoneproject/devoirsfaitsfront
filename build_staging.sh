9703a2d577ea46b04d12aaa06c36dce50f861454
richard <richard@zaclys.net>
2018-07-03 13:03:05 +0200

    sss

--- build_staging.sh
+++ build_staging.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "-- BEGINNING STAGING CUSTOM BUILD SCRIPT EXECUTION FOR DEVOIRSFAITS --"
-echo "-- ->npm ng build --configuration=staging --base-href=/"
+echo "-- ->npm ng build --configuration=staging"
 ng build --configuration=staging --base-href=/
-echo "-- END npm ng build --configuration=staging --base-href=/ --"
+echo "-- END npm ng build --configuration=staging --"
 echo "-- END STAGING CUSTOM BUILD SCRIPT EXECUTION FOR DEVOIRSFAITS --"
 
