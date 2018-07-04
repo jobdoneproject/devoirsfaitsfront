97f47d8f6023a26a930ca4becf5f679df35c5f67
richard <richard@zaclys.net>
2018-07-04 10:54:18 +0200

    removing HTTP protocol to leave HTTPS only

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //import { ProceduralRenderer3 } from '@angular/core/src/render3/interfaces/renderer';
 
 var fs = require('fs');
-var http = require('http');
+//var http = require('http');
 var https = require('https');
 
 var options = {
@@ -24,16 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
-console.log(app.routes);
 
-//
-//app.get('*', app.routes.index);
-//// serve index and view partials
-// app.get('/', routes.index);
-// // redirect all others to the index (HTML5 history)
-// app.get('*', routes.index);
-
-app.get('/', function (req, res) { res.redirect('/index.html') });
 //Gestion du CORS
 //Dans le cas d'une execution de Angular sur un serveur différent du BackEnd !
 app.use(function (req, res, next) {
@@ -43,16 +34,24 @@ app.use(function (req, res, next) {
   next();
 });
 
+//console.log(app.routes);
+//
+//app.get('*', app.routes.index);
+//// serve index and view partials
+// app.get('/', routes.index);
+// // redirect all others to the index (HTML5 history)
+// app.get('*', routes.index);
 
+app.get('/', function (req, res) { res.redirect('/index.html') });
 
 // your express configuration here
-
-var httpServer = http.createServer(app);
+//Removing http only
+//var httpServer = http.createServer(app);
 var httpsServer = https.createServer(options, app);
 
 // Start the app by listening on the default
 // CleverCloud or Heroku port
 
-httpServer.listen(process.env.PORT || 80);
+//httpServer.listen(process.env.PORT || 80);
 httpsServer.listen(8443);
 
